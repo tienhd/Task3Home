4fec529d65c46f99517e7326d0179bda3e46f889
Tien <Hoang>
2013-05-22 10:00:13 +0700

    Hello world git

---
+++ src/main/java/com/qsoft/Calculator.java
@@ -0,0 +1,106 @@
+package com.qsoft;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sqv-nbt
+ * Date: 5/17/13
+ * Time: 8:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+
+
+
+    public int add(String numberString) {
+
+        int result = 0;
+        if (numberString.isEmpty()) {
+            return 0;
+        }
+        else {
+            //String pre = getDefineDelimiterS(numberString);
+            ArrayList<String> pre = getDefineDelimiter(numberString);
+            String regex = " ";
+            if (pre.isEmpty()) {
+                regex = "([,.!\\s+\\n;*/*]*)";
+            }
+            else {
+
+                String[] string = numberString.split("\\n");
+                numberString = string [1];
+
+                for (int i =0; i < pre.size(); i++) {
+
+                    String tempString = pre.get(i);
+                    String temp = "";
+                    for (int j = 0 ; j< tempString.length(); j++) {
+                        char t = tempString.charAt(j);
+                        if ((t == '*') || (t == '?') || (t == '+') || (t =='[') || (t==']') || ( t== '(') || ( t==')')) {
+                            temp += "\\" + t;
+                        }
+                        else {
+                            temp += t;
+                        }
+                    }
+                    //System.out.println("B temp " + temp);
+                    numberString = numberString.replaceAll(temp, " ");
+                }
+                numberString = numberString.replaceAll("[(\\[)(\\])]", " ");
+                regex = " ";
+
+            }
+
+            String[] numbers = numberString.split(regex);
+            for ( int i = 0; i < numbers.length; i++) {
+                if ((!numbers[i].isEmpty()) && (!numbers[i].equals("[")) && (!numbers[i].equals("]"))) {
+                    //System.out.println(numbers[i]);
+                    if (Integer.parseInt(numbers[i]) >= 0) {
+                        result += Integer.parseInt(numbers[i]);
+                    }
+                    else {
+                        throw new NumberFormatException ("Negative is not allowed");
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<String> getDefineDelimiter(String numberString) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        String regex1 = "(//)(\\[(.*)\\])\\n(.*)";
+        //String regex2 = "([)(.*)(])";
+        if (numberString.matches(regex1)) {
+            //System.out.println("MATCH ");
+
+            String[] temp = numberString.split("\\n");
+            String pattern = temp[0];
+            String delimiterString = pattern.substring(2,pattern.length());
+            //System.out.println("DELIMITER " + delimiterString);
+            delimiterString = delimiterString.replaceAll("\\["," ");
+            delimiterString = delimiterString.replaceAll("\\]"," ");
+            String[] splitString = delimiterString.split(" ");
+            for (String si : splitString) {
+                if (!si.isEmpty()) {
+                    result.add(si);
+                }
+            }
+            //System.out.println("HERE " + result);
+            return result;
+        }
+        else {
+            result.clear();
+            return result;
+        }
+    }
+
+    public void testGit() {
+
+    }
+}
+
+
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,68 @@
+import com.qsoft.Calculator;
+import junit.framework.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sqv-nbt
+ * Date: 5/17/13
+ * Time: 8:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void shouldBeZeroOnEmptyString() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void shouldBeThree() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(3,calculator.add("1,2"));
+    }
+
+    @Test
+    public void shouldBeFive() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(5,calculator.add("1,\n2,2"));
+    }
+
+    @Test
+    public void testDifferentDelimiter() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(5,calculator.add("//;1,*\n2,2"));
+    }
+
+    public void testAdvanceDelimiter() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(9,calculator.add("//;\n2;4;3"));
+    }
+
+    @Rule
+    public ExpectedException myException = ExpectedException.none();
+
+    @Test (expected = NumberFormatException.class)
+    public void testException() {
+        Calculator calculator = new Calculator();
+        calculator.add("-1,-2");
+        myException.expect(NumberFormatException.class);
+        myException.expectMessage("Negative is not allowed");
+    }
+
+    @Test
+    public void testWithDefineDelimiter() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(6,calculator.add("//[**6*]\n1**6*2**6*3"));
+        Assert.assertEquals(6,calculator.add("//[**[]*]\n1**[]*2**[]*3"));
+    }
+
+    @Test
+    public void testAdvanceDefineDelimiter() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(6,calculator.add("//[[]*][?]\n1[]*2?3"));
+    }
+}
